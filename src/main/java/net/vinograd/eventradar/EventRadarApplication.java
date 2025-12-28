a08d6226934a02d3ef5963fa781f28b21756487c
vinograd <vinograd1020gm@gmail.com>
2025-12-28 20:56:15 +0300

    Initial commit

---
+++ src/main/java/net/vinograd/eventradar/EventRadarApplication.java
@@ -0,0 +1,13 @@
+package net.vinograd.eventradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRadarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventRadarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/vinograd/eventradar/EventRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vinograd.eventradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRadarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
